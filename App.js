8dd7138aa2a8e363c4f3a7f84bb380dbacda99fc
anandprasad <anandreddy0020@gmail.com>
2022-06-16 15:06:12 +0530

    start coding

--- App.js
+++ App.js
@@ -1,15 +1,38 @@
-import { View, Text } from "react-native";
+import React from "react";
+import { 
+  StyleSheet, View, 
+  Text,Image, SafeAreaView, TouchableOpacity, TouchableNativeFeedback, Button, Platform, StatusBar
+} from "react-native";
 
+import { useDimensions, useDeviceOrientation } from "@react-native-community/hooks";
 export default function App() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+    <SafeAreaView style={styles.container}
     >
-      <Text>Universal React with Expo</Text>
-    </View>
+      <Text>Power of a Tiger</Text>
+      <TouchableOpacity>
+        <Image 
+          fadeDuration={5000}
+          source={{
+            width:200,
+            height:300,
+            uri:"https://picsum.photos/200/300"
+          }}/>
+      </TouchableOpacity>
+      <TouchableNativeFeedback>
+        <View style={{ width:200, height:50, backgroundColor:"dodgerblue", margin:5}}>
+        </View>
+      </TouchableNativeFeedback>
+      <Button color="orange" title="Explore" onPress={()=>alert('button tapped')}/>
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    backgroundColor:"lightblue",
+    alignItems: "center",
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0
+  }
+});
